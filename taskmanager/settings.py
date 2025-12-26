35419c2f1e8a66ae2feb694246961804e5beb79b
BlurryShady <blurryshady@hotmail.com>
2025-12-26 17:02:37 +0300

    Few changes for Brevo

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -185,7 +185,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", "true")
-
+EMAIL_USE_SSL = env_bool("EMAIL_USE_SSL", "false")
+EMAIL_TIMEOUT = 20
 SITE_NAME = "Task Manager"
 
 
